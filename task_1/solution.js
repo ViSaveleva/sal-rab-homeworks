189e81c799e9587bb5cbde98e28c192c639752cb
vsaveleva@azbukavkusa.ru <vsaveleva@azbukavkusa.ru>
2022-06-09 09:53:35 +0300

    task1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,15 +2,12 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
     let message = productName;
-    message = message + " ";
-    message = message + "за";
-    message = message + " ";
-    message = message + "productPrice";
-    message = message + " ";
-    message = message + "теперь в корзине!";
-    console.log (productName);
-    console.log (productPrice);
-    console.log (message);
+    message += " ";
+    message += "за";
+    message += " ";
+    message += productPrice;
+    message += " ";
+    message += "теперь в корзине!";
 
     // Задание №1.1. Оповещение о добавлении в корзину
     // создайте переменную message
@@ -32,8 +29,6 @@ function addInCartCountChange(value) {
     let oldValue = value;
     let newValue = oldValue;
     newValue += 1;
-    console.log (newValue);
-    console.log (oldValue)
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -49,10 +44,6 @@ function addInCartSumChange(sum, delta) {
     let newSum = oldSum + difference;
     let newSumText = newSum + ' ₽';
     newSumText = `${newSum} ₽`;
-    console.log (oldSum);
-    console.log (difference);
-    console.log (newSum);
-    console.log (newSumText);
     // Задание №1.3. Увеличение суммы в корзине
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
