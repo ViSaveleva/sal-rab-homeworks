59019ea6547130d7f9edb177b85ee6d8179f80d5
vsaveleva@azbukavkusa.ru <vsaveleva@azbukavkusa.ru>
2022-06-26 12:13:30 +0300

    task2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,7 +2,7 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
-
+ 
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
@@ -20,11 +20,11 @@ function calcShipping(sum, min, shipping) {
 
     return shippingSum;
 }
-
 function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
+
 
     // Задание №2.2. Рассчитать скидку
 
