0d3bbdd68c9160c924af30fad3290a6c10934cb6
vsaveleva@azbukavkusa.ru <vsaveleva@azbukavkusa.ru>
2022-07-16 12:55:34 +0300

    task3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -5,7 +5,7 @@ function renderCartItem(item) {
     itemCountText = itemCountText + "×";
     itemCountText = itemCountText + product.price;
     itemCountText = itemCountText + "₽ =";
-    let sum = product.count * product.price;
+    let sum = product.count*product.price;
     itemCountText = itemCountText + sum;
     itemCountText = itemCountText + "₽";
 
